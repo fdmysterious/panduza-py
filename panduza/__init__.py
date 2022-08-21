da350647905ca17513ca80cdc53731748b042355
xdoctorwhoz <xavier.rodriguezalvarez@gmail.com>
2022-08-21 21:43:58 +0200

    docker env ok

--- panduza/__init__.py
+++ panduza/__init__.py
@@ -4,11 +4,6 @@ from .core import Interface
 from .core import EnsureError
 
 from .interfaces import Io
+from .interfaces import Psu
 from .interfaces import File
-from .interfaces import PowerSupply
-# from .interfaces.serial import Serial
-# from .interfaces.spi_master import SpiMaster
-# from .interfaces.spi_slave import  SpiSlave
-# from .interfaces.twi_master import TwiMaster
-# from .interfaces.twi_slave import  TwiSlave
 
