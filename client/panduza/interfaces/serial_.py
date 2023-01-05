4ec65967f946e27cef85ae8e3a7c7349b0e82fe4
xdoctorwhoz <xavier.rodriguezalvarez@gmail.com>
2023-01-05 10:58:11 +0100

    append read feature on the serial client

--- client/panduza/interfaces/serial_.py
+++ client/panduza/interfaces/serial_.py
@@ -9,12 +9,29 @@ from ..core import Interface, Attribute, EnsureError, RoField, RwField
 
 @dataclass
 class AttributeData(Attribute):
-    _rx_buffer: bytes = None
+    _rx_buffer: bytes = b''
 
 
     def _on_att_message(self, topic, payload):
         self._log.debug("overload !!!")
 
+        payload_dict = json.loads(payload.decode("utf-8"))
+        self._log.debug(f"{payload_dict}")
+
+        if 'rx' in payload_dict['data']:
+            rx_string = payload_dict['data']['rx']
+            base64_bytes = rx_string.encode('ascii')
+            message_bytes = base64.b64decode(base64_bytes)
+
+            self._rx_buffer += message_bytes
+        
+            self._log.debug(f"{self._rx_buffer}")
+
+
+    def read(self, size = None):
+        data = self._rx_buffer[:size]
+        self._rx_buffer = self._rx_buffer[len(data):]
+        return data
 
 
 
@@ -47,11 +64,13 @@ class Serial(Interface):
         )
 
 
-    def send(self, tx_data: bytes):
+    def write(self, tx_data: bytes):
         tx_encoded = base64.b64encode(tx_data)
         base64_message = tx_encoded.decode('ascii')
         self.data.tx.set(base64_message)
 
 
+    def read(self, size = None):
+        return self.data.read(size)
 
 
