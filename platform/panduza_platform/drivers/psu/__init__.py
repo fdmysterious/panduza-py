2fa19201e555512bee29796dd7c99933437743ff
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2023-01-11 07:03:20 +0000

    Add basic driver for RS Pro IPS4303S

--- platform/panduza_platform/drivers/psu/__init__.py
+++ platform/panduza_platform/drivers/psu/__init__.py
@@ -1,10 +1,12 @@
 from .aimtti.driver_ql355p import DriverQL355P
 
 from .hanmatek.hm310t_driver import DriverHM310T
+from .rspro.driver_4303s     import Driver4303S
 
 PZA_DRIVERS_LIST=[
     DriverQL355P,
-    DriverHM310T
+    DriverHM310T,
+    Driver4303S,
 ]
 
 
