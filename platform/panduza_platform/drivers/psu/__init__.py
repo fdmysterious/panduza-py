1784209177797eb6190af2eaa633db5e6ae04c8a
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2023-01-12 10:21:01 +0100

    Fix module init

--- platform/panduza_platform/drivers/psu/__init__.py
+++ platform/panduza_platform/drivers/psu/__init__.py
@@ -1,12 +1,12 @@
 from .aimtti.driver_ql355p import DriverQL355P
 
 from .hanmatek.hm310t_driver import DriverHM310T
-from .rspro.driver_4303s     import Driver4303S
+from .rspro.driver_4303s     import DriverIPS4303S
 
 PZA_DRIVERS_LIST=[
     DriverQL355P,
     DriverHM310T,
-    Driver4303S,
+    DriverIPS4303S
 ]
 
 
