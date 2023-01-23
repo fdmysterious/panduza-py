0cb7b8ba1e09d9234e34a86a67958318eb09ea80
xdoctorwhoz <xavier.rodriguezalvarez@gmail.com>
2023-01-23 14:51:30 +0100

    fix robof psu

--- client/panduza/robof/psu.py
+++ client/panduza/robof/psu.py
@@ -6,6 +6,13 @@ from robot.libraries.BuiltIn import BuiltIn
 class KeywordsPsu(object):
 
     @keyword
+    def set_power_supply_voltage(self, name, voltage):
+        """
+        """
+        pza = BuiltIn().get_variable_value("${__pza__}")
+        pza[name].volts.value.set(float(voltage))
+
+    @keyword
     def turn_on_power_supply(self, name):
         """Turn on the psu
         """
@@ -13,12 +20,15 @@ class KeywordsPsu(object):
         pza[name].state.value.set("on")
 
     @keyword
-    def turn_off_power_supply(self, name):
+    def turn_off_power_supply(self, name, teardown=False):
         """Turn on the psu
         """
         pza = BuiltIn().get_variable_value("${__pza__}")
-        pza[name].state.value.set("off")
 
-
+        if pza == None:
+            # It is ok if panduza is not initialized, only if in the teardown process
+            assert not teardown
+        else:
+            pza[name].state.value.set("off")
 
 
